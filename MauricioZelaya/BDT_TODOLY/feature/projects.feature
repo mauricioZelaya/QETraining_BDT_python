42199a9356285ad882a30b8e9958e6487ecf8c89
Daneiva Gamboa Ortuno <Daneiva Gamboa@jalasoft.local>
2017-11-23 11:50:17 -0400

    updating Projects files

--- MauricioZelaya/BDT_TODOLY/feature/projects.feature
+++ MauricioZelaya/BDT_TODOLY/feature/projects.feature
@@ -3,30 +3,31 @@ Feature: This feature is to test the implementation of the projects management f
 
   @smoke_test
   Scenario Outline: This scenario tests the basic status_code response from API projects management services.
-    When I send a <method> request to <service> with <format> format
-    Then I get status code <status_code>
+    When I send a <method> for projects request to <service> with <format> format
+    Then Getting status code project operation <status_code>
 
     Examples:
-      | method | service     | format | status_code |
-      | GET    | /projects   | .json  | 200         |
-      | DELETE | /projects/0 | .json  | 400         |
+      | method | service   | format | status_code |
+      | GET    | /projects | .json  | 200         |
+     # | DELETE | /projects/0 | .json  | 400         |
+
 
   @crud_test
   @acceptance
-  Scenario Outline: Confirm that a project can be updated
-    When I post the following body in <format> format to update the project with ID <ID>:
+    #CREATE
+  Scenario Outline: Confirm that a project can be added
+    When I <method> the following body in <format> format to add the project with name <name>:
   """
-      {
-          "ProjectObject": {
-              "Content": "ProjectNameModified1",
-              "Icon": "4"
-          }
-      }
+        {
+            "ProjectObject": {
+              "Content": "<name>"
+            }
+        }
       """
-    Then I get status code <status_code>
-    And the edited project with the new data.
+    Then I get project code result <status_code>
+    And the new project is added.
 
     Examples:
-      | ID      | format | status_code |
-      | 3354231 | .json  | 200         |
-      | 9900000 | .json  | 400         |
+      | method | name     | format | status_code |
+      | POST   | project1 | .json  | 200         |
+     # | none       | .json  | 400         |
--- MauricioZelaya/BDT_TODOLY/steps/projects_steps.py
+++ MauricioZelaya/BDT_TODOLY/steps/projects_steps.py
@@ -1,13 +1,44 @@
-@when(u'I post the following body in .json format to update the project with ID 3354231')
+from utils.utils import *
+from compare import expect
+from behave import when, then
+import logging
+
+
+logging.basicConfig(filename='..\logs\projects.log', level=logging.INFO)
+
+@when(u'I send a {method} for projects request to {service} with .json format')
+def step_impl(context, method, service):
+    context.method = method
+    context.service = service
+
+@then(u'Getting status code project operation {code}')
+def step_impl(context, code):
+    context.endpoint = create_endpoint(context.endpoint, context.service)
+    authorization = fill_authorization_basic(context.__DAN_USER__, context.__DAN_PASS__)
+    expect(int(code)).to_equal(get_conn(context.endpoint, context.method, auth=authorization))
+
+@when(u'I {method} the following body in {format} format to add the project with name {name}')
+def step_impl(context, method, name, format):
+    authorization = fill_authorization_basic(context.__DAN_USER__, context.__DAN_PASS__)
+    context.method = method
+    context.projectName = name
+    context.service = "projects"
+    context.payload = {"Content": name}
+    context.format = format
+    logging.info('------------endpoint---------------')
+    context.endpoint = create_endpoint(context.endpoint, context.service)
+    logging.info(context.endpoint)
+    logging.info('----------------payload-----------')
+    logging.info(repr(context.payload))
+    new_project = requested_method(context.endpoint, context.payload, context.method,auth=authorization)
+    logging.info('----------------project-----------')
+    logging.info(repr(new_project))
+
+@then(u'I get project code result 200')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I post the following body in .json format to update the project with ID 3354231')
+    pass
 
-@then(u'the edited project with the new data.')
+
+@then(u'the new project is added.')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the edited project with the new data.')
-
-@when(u'I post the following body in .json format to update the project with ID 9900000')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When I post the following body in .json format to update the project with ID 9900000')
-
-
+    pass
