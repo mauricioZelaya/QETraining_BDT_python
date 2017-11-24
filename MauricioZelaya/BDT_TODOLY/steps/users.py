113d9d1ed70e0907661bf06c0b6f905bef869d67
Mauricio Zelaya <zelaya.mauricio@gmail.com>
2017-11-24 09:45:44 -0400

    test

--- MauricioZelaya/BDT_TODOLY/steps/users.py
+++ MauricioZelaya/BDT_TODOLY/steps/users.py
@@ -19,9 +19,10 @@ def step_impl(context, code):
     expect(statusCodeReceived).to_equal(int(code))
 
 
-@given(u'{format} format for the response')
-def step_impl(context, format):
-    context.format = format
+@given(u'an authenticated user {email} and {firstName} {lastName}')
+def step_impl(context, email, firstName, lastName):
+    context.email = email
+    context.fullName = firstName + " " + lastName
 
 
 @when(u'I {method} the {service} service method to request user information')
@@ -36,13 +37,40 @@ def step_impl(context, code):
     expect(get_conn(context.endpoint, context.method, auth=context.auth)).to_equal(int(code))
 
 
-
-@then(u'I compare result')
+@then(u'I compare result to validate the mail and fullname are aslo received')
 def step_impl(context):
-   # context.text = text
-    response = get_response(context.endpoint, context.method, auth=context.auth)
-    print(response)
-    newJson = serialyze_json(response, 'zelaya.mauricio@gmail.com')
-    print(newJson)
-    #expect(response).to_equal(context.text)
+    context. response = get_response(context.endpoint, context.method, auth=context.auth)
+    # print(context.email)
+    # print(context.fullName)
+    # print(context.response)
+    expect(True).to_equal(is_item_in_the_response("Email", context.email, context.response))
+    expect(True).to_equal(is_item_in_the_response("FullName", context.fullName, context.response))
+
+
+@given(u'an authenticated user with {userEmail} email')
+def step_impl(context, userEmail):
+    context.Email = userEmail
+
+
+@when(u'I {method} request to change the full name to {newFirstName} {newLastName} using {service} service')
+def step_impl(context, method, newFirstName, newLastName, service):
+    context.method = method
+    context.service = service
+    context.fullName = newFirstName + " " + newLastName
+    context.json_data = {"Email": context.Email, "FullName": context.fullName}
+    print(context.json_data)
+
+
+@then(u'I get response code {code}')
+def step_impl(context, code):
+    context.endpoint = create_endpoint(context.endpoint, context.service)
+    expect(get_conn(context.endpoint, method=context.method, json_data=context.json_data, auth=context.auth)).to_equal(int(code))
+
+
+@then(u'The new name is part of the response')
+def step_impl(context):
+    context.response = get_response(context.endpoint, context.method, auth=context.auth)
+    print(context.response)
+    expect(True).to_equal(is_item_in_the_response("FullName", context.fullName, context.response))
+
 
